1fb62595a771c0356a0015a64ec0aa779482a987
namilan <nichole.milan@gmail.com>
2019-06-12 13:51:02 -0400

    review class

--- src/main/java/org/wecancodeit/reviewssite/Review.java
+++ src/main/java/org/wecancodeit/reviewssite/Review.java
@@ -3,14 +3,57 @@ package org.wecancodeit.reviewssite;
 public class Review {
 	
 	private long id;
+	private String title;
+	private String imageURL;
+	private String reviewCategory;
+	private String content;
+	private String date;
+	private String synopsis;
+	private String tag;
+	
 
-	public Review(long id, String string, String string2, String string3, String string4, String string5,
-			String string6, String string7) {
+	public Review(long id, String title, String imageURL, String reviewCategory, String content, String date,
+			String synopsis, String tag) {
 		this.id = id;
+		this.title = title;
+		this.imageURL = imageURL;
+		this.reviewCategory = reviewCategory;
+		this.content = content;
+		this.date = date;
+		this.synopsis = synopsis;
+		this.tag = tag;
 	}
 
 	public Long getId() {
 		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getImageURL() {
+		return imageURL;
+	}
+	
+	public String getReviewCategory(){
+		return reviewCategory;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public String getSynopsis() {
+		return synopsis;
+	}
+	
+	public String getTag() {
+		return tag;
 	}
 
 
