81546668096fab7157cf37d9b77e1927c3dc8e7b
namilan <nichole.milan@gmail.com>
2019-06-12 15:39:05 -0400

    updated files

--- src/main/java/org/wecancodeit/reviewssite/ReviewsController.java
+++ src/main/java/org/wecancodeit/reviewssite/ReviewsController.java
@@ -1,5 +1,28 @@
 package org.wecancodeit.reviewssite;
 
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class ReviewsController {
 
+	@Resource
+	ReviewRepository repository;
+
+		
+	@RequestMapping("/reviews")
+	public String fetchReviews(Model model) {
+		model.addAttribute("reviewsAsCollection", repository.findAll());
+		return "reviews";
+	}
+	
+	@RequestMapping("/onereview")
+    public String fetchOne(@RequestParam(value="id") Long id, Model model) {
+         model.addAttribute("onereview", repository.findOne(id));
+         return "onereview";
+     }
 }
